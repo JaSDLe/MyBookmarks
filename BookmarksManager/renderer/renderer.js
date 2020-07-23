7947b657eeb02cad0b550e7591c8d9cb2f05ebd8
Jason Lee <jasdlee@outlook.com>
2020-07-24 01:57:05 +0800

    update

--- BookmarksManager/renderer/renderer.js
+++ BookmarksManager/renderer/renderer.js
@@ -15,77 +15,41 @@ function a() {
   // alert('a')
 }
 
-// const fs = require("fs");
-
-// const dragWrapper = document.getElementById("drag_test");
+const fs = require('fs')
 
 let result = new Array()
 
-// setTimeout(add(), 3000)
+let intervalID
+intervalID = setInterval(addEventListenerForDrag, 1)
 
-function add() {
-  console.log(new Date())
-  console.log('add event')
+function addEventListenerForDrag() {
+  const dragWrapper = document.getElementById("drag_test")
+  if (dragWrapper != null) {
+    add(dragWrapper)
+    clearInterval(intervalID)
+  }
+}
 
-  const dragWrapper = document.getElementById("drag_test");
-  console.log(dragWrapper)
-
-  dragWrapper.addEventListener("click", (e) => {
-    console.log('click')
-    e.preventDefault();
-  })
-  dragWrapper.addEventListener("drop", (e) => {
-    console.log('drop')
-    e.preventDefault();
-    const files = e.dataTransfer.files;
+function add(element) {
+  element.addEventListener("drop", (e) => {
+    e.preventDefault()
+    const files = e.dataTransfer.files
+    console.log(e)
     console.log(files)
-    const fs = require("fs")
     console.log(fs)
     if (files && files.length >= 1) {
       const doc = (new DOMParser()).parseFromString(fs.readFileSync(files[0].path, 'utf-8'), "text/html")
-      const dl = doc.body.children[1]
-      console.log(dl)
       let content = doc.body.children[1].children[1].children[1].children
-      // for (let i = 1; i < content.length; i++) {
-      //   content[i]
-      //   result.push()
-      //   addChild(content[i], result)
-      // }
       console.log(content)
       loopArray(content, result)
       console.log(result)
       document.getElementById("result").innerText = result
     }
   })
-  dragWrapper.addEventListener("dragover", (e) => {
-    console.log('dragover')
-    e.preventDefault();
+  element.addEventListener("dragover", (e) => {
+    e.preventDefault()
   })
 }
-// dragWrapper.addEventListener("drop", (e) => {
-//   e.preventDefault();
-//   const files = e.dataTransfer.files;
-//   if (files && files.length >= 1) {
-//     const doc = (new DOMParser()).parseFromString(fs.readFileSync(files[0].path, 'utf-8'), "text/html")
-//     const dl = doc.body.children[1]
-//     console.log(dl)
-//     let content = doc.body.children[1].children[1].children[1].children
-//     // for (let i = 1; i < content.length; i++) {
-//     //   content[i]
-//     //   result.push()
-//     //   addChild(content[i], result)
-//     // }
-//     console.log(content)
-//     loopArray(content, result)
-//     console.log(result)
-//     document.getElementById("result").innerText = result
-//   }
-// })
-
-//这个事件也需要屏蔽
-// dragWrapper.addEventListener("dragover", (e) => {
-//   e.preventDefault();
-// })
 
 function loopArray(array, parentArray) {
   for (let i = 0; i < array.length; i++) {
@@ -93,7 +57,8 @@ function loopArray(array, parentArray) {
   }
 }
 
-const suffix = '000'
+const suffix = '0'
+const timestampLength = 13
 
 function addChild(child, array) {
   switch (child.tagName) {
@@ -101,35 +66,15 @@ function addChild(child, array) {
     case 'DT':
       array.push(new Array())
       loopArray(child.children, array[array.length - 1])
-      break;
+      break
     case 'H3':
-      // console.log('hhhhhhhhhhhhhhhhhhhhhhhh')
-      // console.log(child.attributes)
-      array.push(new H(child.attributes.add_date.value.concat(suffix), child.attributes.last_modified.value.concat(suffix), child.innerText))
-      break;
+      pushElement(new H(), child, array)
+      break
     case 'A':
-      // console.log('aaaaaaaaaaaaaaaaaaaaaaaaaa')
-      // console.log(child.attributes)
-      // if (child.attributes.length == 2) {
-      //   array.push(new A(child.attributes.href.value, child.attributes.add_date.value.concat(suffix), '', child.innerText))
-      // } else {
-      //   array.push(new A(child.attributes.href.value, child.attributes.add_date.value.concat(suffix), child.attributes.icon.value, child.innerText))
-      // }
-      let a = new A()
-      child.attributes.forEach(item => {
-        if (a.hasOwnProperty(item.name)) {
-          if (item.name === 'add_date' || item.name === 'last_modified') {
-            a[map.get(item.name)] = item[item.name].value.concat(suffix)
-          } else {
-            a[map.get(item.name)] = item[item.name].value
-          }
-        }
-      })
-      a.value = child.innerText
-      array.push(a)
-      break;
+      pushElement(new A(), child, array)
+      break
     default:
-      break;
+      break
   }
 }
 
@@ -139,31 +84,37 @@ map.set('last_modified', 'lastModified')
 map.set('href', 'href')
 map.set('icon', 'icon')
 
-
-
-function H(addDate, lastModified, value) {
-  this.addDate = addDate
-  this.lastModified = lastModified
-  this.value = value
+function pushElement(element, child, array) {
+  for (const key in child.attributes) {
+    if (child.attributes.hasOwnProperty(key)) {
+      const item = child.attributes[key]
+      const property = map.get(item.name)
+      if (element.hasOwnProperty(property)) {
+        element[property] = getValue(item)
+      }
+    }
+  }
+  element.value = child.innerText
+  array.push(element)
 }
 
-function A(href, addDate, icon, value) {
-  this.href = href
-  this.addDate = addDate
-  this.icon = icon
-  this.value = value
+function getValue(item) {
+  return (item.name === 'add_date' || item.name === 'last_modified') && item.value.length < timestampLength ? item.value.concat(suffix.repeat(timestampLength - item.value.length)) : item.value
 }
 
-function Car(sColor, iDoors, iMpg) {
-  this.color = sColor;
-  this.doors = iDoors;
-  this.mpg = iMpg;
-  this.drivers = new Array("Mike", "John");
+class H {
+  constructor(addDate, lastModified, value) {
+    this.addDate = addDate
+    this.lastModified = lastModified
+    this.value = value
+  }
+}
 
-  if (typeof Car._initialized == "undefined") {
-    Car.prototype.showColor = function () {
-      alert(this.color);
-    };
-    Car._initialized = true;
+class A {
+  constructor(href, addDate, icon, value) {
+    this.href = href
+    this.addDate = addDate
+    this.icon = icon
+    this.value = value
   }
 }
